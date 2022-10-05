7cfe7a52eec219f8496715b1d51608072f2bfa12
LouieAlcantara <luna.alcantara19@gmail.com>
2022-10-05 18:27:47 -0300

    funcao_linha

---
+++ funcao_linha.py
@@ -0,0 +1,18 @@
+def linha(txt):
+    print('-'*30)
+    print(txt)
+    print('-'*30)
+
+linha('  Home  ')
+linha(' contatos')
+linha('  exit   ')
+
+
+def soma(x,y):
+    s=x+y
+    print(s)
+
+soma(3,8)
+soma(9,10)
+
+
