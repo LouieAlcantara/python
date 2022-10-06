78b055e8d2d228159e2919a46e7facc3e6f1ead8
LouieAlcantara <luna.alcantara19@gmail.com>
2022-10-06 09:11:43 -0300

    terreno

---
+++ conta_corrente.py
@@ -0,0 +1,17 @@
+
+conta={
+    'nome':input('Seu nome completo:' ),
+    'conta': int(12345),
+    'cliente': int(1),
+    'saldo': 'saldo'
+}
+def di(txt):
+    print('-'*100)
+    print(txt)
+    print('-'*100)
+
+di(conta)
+
+#def deposito (saldo,valor):
+    #print( saldo + valor)
+#acho que o saldo começa a partir de funçoes tipo essa, por isso deixei como comentario
---
+++ terrenos.py
@@ -0,0 +1,14 @@
+def a (larg,com):
+    w= larg*com
+    print(f'a area de {x}x{y}, é {w}')
+
+
+
+
+print('-'*30)
+print('controle de terreno')
+print('-'*30)
+print('')
+x = float(input('Lagura em metros: '))
+y = float(input('Comprimento em metros: '))
+a(x, y)
