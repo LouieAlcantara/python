0bdaaf7a0a7add5f856292d8642252bf9ab7de95
LouieAlcantara <luna.alcantara19@gmail.com>
2022-10-20 15:36:03 -0300

    corrigindo_erros_2.0

--- Jogo_ppt.py
+++ Jogo_ppt.py
@@ -2,15 +2,18 @@ import def_ppt
 import random
 import time
 
+r ='S'
 while( True):
     def_ppt.bem_vindo('OLÁ, XD, Vamos jogar pedra, papel e tesoura?')
-    r = str(input('Deseja continuar ? [S/N] ')).upper
-    while(r == 'S'):
-        print('Vamos nessa')
-    if (r != 'S'):
+    r = str(input('Deseja continuar ? [S/N] ')).upper()
+    if (r == 'S'):
+        print('Vamos continuar')
+    elif(r != 'N'):
+        break
+    else:
         break
     escolha = int(input('Escolha [1] para pedra, [2] para papel e [3] para tesoura '))
-    if(escolha >= 3):
+    if(escolha > 3):
         print('ERROR')
         break
     elif(escolha <= -1):
@@ -36,26 +39,26 @@ while( True):
         if (escolha == 1):
             print('Houve empate')
         elif (escolha == 2):
-            print('Você perdeu')
-        elif (escolha == 3):
             print('Você ganhou')
+        elif (escolha == 3):
+            print('Você perdeu')
         
     elif (computador == 2):
         print('O computador escolheu : PAPEL')
         if (escolha == 2):
             print('Houve empate')
         elif (escolha == 3):
-            print('Você perdeu')
+            print('Você ganhou')
         elif (escolha == 1):
-            print('Você ganhou') 
+            print('Você perdeu') 
 
     elif (computador == 3):
         print('O computador escolheu : TESOURA')
         if (escolha == 3):
             print('Houve empate')
         elif (escolha == 1):
-            print('Você perdeu')
-        elif (escolha == 2):
             print('Você ganhou')
+        elif (escolha == 2):
+            print('Você perdeu')
     else:
         break
